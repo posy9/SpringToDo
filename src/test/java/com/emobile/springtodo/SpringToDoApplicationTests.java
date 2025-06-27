f4de2e8aba26b5894d0727ef06ca7db6261a0188
posy9 <dannildtannk@gmail.com>
2025-06-27 13:16:55 +0300

    context loads test added

--- src/test/java/com/emobile/springtodo/SpringToDoApplicationTests.java
+++ src/test/java/com/emobile/springtodo/SpringToDoApplicationTests.java
@@ -1,13 +1,56 @@
 package com.emobile.springtodo;
 
+
+
+import com.emobile.springtodo.controller.TaskController;
+import com.emobile.springtodo.controller.UserController;
+import com.emobile.springtodo.repository.TaskRepository;
+import com.emobile.springtodo.repository.UserRepository;
+import com.emobile.springtodo.service.TaskService;
+import com.emobile.springtodo.service.UserService;
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@Testcontainers
 class SpringToDoApplicationTests {
+
+    @Container
+    @ServiceConnection
+    public static PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:16");
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TaskRepository taskRepository;
+    @Autowired
+    private TaskService taskService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserController userController;
+    @Autowired
+    private TaskController taskController;
+
+
 
     @Test
     void contextLoads() {
+        assertNotNull(userRepository);
+        assertNotNull(taskRepository);
+        assertNotNull(taskService);
+        assertNotNull(userService);
+        assertNotNull(userController);
+        assertNotNull(taskController);
     }
 
 }
